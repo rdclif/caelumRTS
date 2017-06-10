fcfeabb666a3a190fb0da1ad5f396960d29fc4b1
Royce Hwang <hwangr@flip1.engr.oregonstate.edu>
2017-06-09 17:37:53 -0700

    Removed unnecessary file AI_decision.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -98,7 +98,7 @@ module.exports = function(grunt) {
     connect: {
       server: {
         options: {
-          port: 8080,
+          port: 9080,
           keepalive: false
         }
       }
